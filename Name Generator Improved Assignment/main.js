d4462ac6788dbd77bf06cac2526514236f24a429
Yurqles <57115688+Yurqles@users.noreply.github.com>
2020-09-21 14:11:11 -0600

    Add files via upload

---
+++ Name Generator Improved Assignment/main.js
@@ -0,0 +1,51 @@
+//Name Generator
+
+//Name Placement
+let pEl = document.getElementById('p1');
+let breakLine = document.getElementById('hr1');
+let namePaths;
+let nameIndex = 0;
+
+//Event Listener
+document.getElementById('btn1').addEventListener('click', randomName);
+document.getElementById('btn2').addEventListener('click', allNames);
+
+fetch('nicknames.txt').then(convertData).then(processData);
+
+function convertData(rawData) {
+    //Get the nicknames from the text file and covert it into an array of characters
+    return rawData.text;
+}
+
+function processData(data) {
+    namePaths = data.split('\r\n');
+}
+
+function allNames (aString) {
+    //Get the first and last name
+    let firstName = document.getElementById('textInput1').value;
+    let lastName = document.getElementById("textInput2").value;
+
+    //Get strings
+    let namePaths = aString.split("");
+
+    //Add all names
+    for (let i = 0; i < namePaths.length; i++) {
+        return firstName + namePaths.join("") + lastName;
+    }
+} 
+
+function randomName(aString) { 
+    //Get the first and last name
+    let firstName = document.getElementById('textInput1').value;
+    let lastName = document.getElementById("textInput2").value;
+
+    //Get strings
+    let namePaths = string.split("");
+
+    //Get a random index
+    nameIndex = Math.randomInt(0, namePaths.length); 
+
+    //Display the new nickname
+    return firstName + namePaths.join("") + lastName;
+}
